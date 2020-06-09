9ea0343c396c6b138201200305a2157eeda53369
Matheus <matheus@e-com.club>
2020-06-09 14:57:48 -0300

    chore: update checkout

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -7,24 +7,31 @@ var lastphrase = document.getElementById('noMore')
 var lessQuantity = 12
 lessUnit.innerHTML = lessQuantity
 ecomCart.on('change', ({ data }) => {
-  var checkoutButton = document.querySelector('.cart__btn-checkout')
-  var percentBar
-  var countQuantity = data.items.reduce((acc, curr) => acc + curr.quantity, 0)
-  var evalQuantity = lessQuantity - countQuantity
-  if (evalQuantity > 0) {
-    lessUnit.innerHTML = evalQuantity
-    percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
-    document.getElementById('lastUnitsBar').style.width = percentBar
-    document.getElementById('percentBar').innerHTML = percentBar
-    firstphrase.style.display = 'block'
-    lastphrase.style.display = 'none'
-    checkoutButton.style.display = 'none'
+  var cartCalc = document.querySelectorAll('#cart')
+  if (cartCalc.length) {
+    document.getElementById('containerCalc').style.display = 'block'
+    var checkoutButton = document.querySelector('.cart__btn-checkout')
+    var percentBar
+    console.log(data)
+    var countQuantity = data.items.reduce((acc, curr) => acc + curr.quantity, 0)
+    var evalQuantity = lessQuantity - countQuantity
+    if (evalQuantity > 0) {
+      lessUnit.innerHTML = evalQuantity
+      percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+      firstphrase.style.display = 'block'
+      lastphrase.style.display = 'none'
+      checkoutButton.style.display = 'none'
+    } else {
+      percentBar = '100%'
+      checkoutButton.style.display = 'block'
+      firstphrase.style.display = 'none'
+      lastphrase.style.display = 'block'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+    }
   } else {
-    percentBar = '100%'
-    checkoutButton.style.display = 'block'
-    firstphrase.style.display = 'none'
-    lastphrase.style.display = 'block'
-    document.getElementById('lastUnitsBar').style.width = percentBar
-    document.getElementById('percentBar').innerHTML = percentBar
+    document.getElementById('containerCalc').style.display = 'none'
   }
 })
