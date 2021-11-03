90feed4db65a653b478ddd347430c5723dc75532
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-11-03 14:16:12 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,2 +1,9 @@
 // Add your custom JavaScript for storefront pages here.
 window.$ecomConfig.set('default_img_size', 'zoom')
+window.storefront.on('widget:@ecomplus/widget-minicart', function () {
+  setTimeout(function () {
+    const infoChange = document.querySelector('.info-additional')
+    const buy = document.querySelector('.product__buy')
+    buy.after(infoChange)
+  }, 1000)
+})
