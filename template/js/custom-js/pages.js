cc5701960a6b9e5cab3dd5e363645236eb661207
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-11-16 11:56:21 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -3,7 +3,12 @@ window.$ecomConfig.set('default_img_size', 'zoom')
 window.storefront.on('widget:@ecomplus/widget-minicart', function () {
   setTimeout(function () {
     const infoChange = document.querySelector('.info-additional')
-    const buy = document.querySelector('.product__buy')
-    buy.after(infoChange)
+    const buy = document.querySelector('.product__buy').product__short-stock
+    const lessProduct = document.querySelectorAll('.product__short-stock')
+    if (lessProduct.length) {
+      lessProduct.after(infoChange)
+    } else {
+      buy.after(infoChange)
+    }
   }, 1000)
 })
