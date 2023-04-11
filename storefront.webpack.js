c67fa0a19415733dcd886d83ca731e4d10b18dd8
matheusgnreis <matheus@e-com.club>
2023-04-11 20:25:44 -0300

    chore: update store

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,7 @@ module.exports = () => ({
     alias: {
       './html/PaymentOption.html': path.resolve(__dirname, 'template/js/custom-js/html/PaymentOption.html'),
       './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js'),
-      './js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/js/SearchEngine.js')
+      //'./js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/js/SearchEngine.js')
       //'./js/Checkout.js': path.resolve(__dirname, 'template/js/views/js/Checkout.js')
     }
   }
