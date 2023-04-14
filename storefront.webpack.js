090a6c9f3f6fd80d48a88ee4ac2fff38d3d1eceb
matheusgnreis <matheus@e-com.club>
2023-04-13 21:30:08 -0300

    chore: update layout

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/PaymentOption.html': path.resolve(__dirname, 'template/js/custom-js/html/PaymentOption.html'),
-      './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js'),
+      './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js')
       //'./js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/js/SearchEngine.js')
       //'./js/Checkout.js': path.resolve(__dirname, 'template/js/views/js/Checkout.js')
     }
