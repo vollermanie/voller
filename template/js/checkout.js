563c6321af178cffb315cf974badb8dcf5b3996f
vollermanie <65929383+vollermanie@users.noreply.github.com>
2023-04-19 09:47:51 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -4,18 +4,18 @@ import ecomCart from '@ecomplus/shopping-cart'
 var lessUnit = document.getElementById('lessUnit')
 var firstphrase = document.getElementById('lessSome')
 var lastphrase = document.getElementById('noMore')
-var lessQuantity = 6
-lessUnit.innerHTML = lessQuantity
+var lessQuantity = 180
+lessUnit.innerHTML = window.ecomUtils.formatMoney(lessQuantity, 'BRL', 'pt_br')
 ecomCart.on('change', ({ data }) => {
   var cartCalc = document.querySelectorAll('#cart')
   if (cartCalc.length) {
     document.getElementById('containerCalc').style.display = 'block'
     var checkoutButton = document.querySelector('.cart__btn-checkout')
     var percentBar
-    var countQuantity = data.items.reduce((acc, curr) => acc + curr.quantity, 0)
+    var countQuantity = data.subtotal
     var evalQuantity = lessQuantity - countQuantity
     if (evalQuantity > 0) {
-      lessUnit.innerHTML = evalQuantity
+      lessUnit.innerHTML = window.ecomUtils.formatMoney(evalQuantity, 'BRL', 'pt_br')
       percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
       document.getElementById('lastUnitsBar').style.width = percentBar
       document.getElementById('percentBar').innerHTML = percentBar
@@ -38,10 +38,10 @@ const router1 = window.storefrontApp && window.storefrontApp.router
 setInterval(function () {
   if (router1) {
     const emitCheckout1 = (name) => {
-      var countQuantity = ecomCart.data.items.reduce((acc, curr) => acc + curr.quantity, 0)
+      var countQuantity = ecomCart.data.subtotal
       if (countQuantity < lessQuantity) {
         window.location.href = '/app/#/cart'
-        window.alert('Apenas ' + countQuantity + ' itens no carrinho. Um deles acabou estoque! Você estará sendo direcionado para o carrinho para inserir mais um item')
+        window.alert('Um dos produtos o estoque acabou! Você estará sendo direcionado para o carrinho para inserir mais itens para pedido mínimo')
       }
     }
 
